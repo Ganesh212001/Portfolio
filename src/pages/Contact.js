d6e6571ff9a98bddf7221f7519bdedf8ae1aef45
Ganesh <ganeshmahajan21062001@gmail.com>
2023-06-05 17:21:14 +0530

    FinaleNine

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -28,8 +28,8 @@ function Contact() {
         </div>
         <div className="flex flex-wrap mb-4">
           <div className="w-full">
-            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline-blue" type="button">
-              Send
+            <button className="primary-button">
+              <span>Submit</span>
             </button>
           </div>
         </div>
