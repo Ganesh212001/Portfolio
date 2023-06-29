979d9c2b36244a16190f44a1a350be33ea8dded4
Ganesh <ganeshmahajan21062001@gmail.com>
2023-06-29 19:34:28 +0530

    icon

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import Projects from './Projects'
 import Contact from './Contact'
 import Footer from '../components/Footer'
 import TypeJS from '../components/TypeJS'
 import './Home.css'
+// import '../components/Footer.css'
 import About from '../pages/About'
 
 const Home = () => {
@@ -15,6 +17,46 @@ const Home = () => {
           <p className="text-lg mb-2">This is my personal portfolio website built with React.</p>
           <p className="text-lg mb-4">Feel free to explore my projects and learn more about me.</p> <br />
           <TypeJS/>
+        </div>
+        <div className='flex justify-end'>
+        <div className="flex flex-col space-y-4 fixed mt-5">
+            <Link
+              className="inline-block mx-2"
+              to="https://www.facebook.com/ganesh.mahajan.92"
+              target="_blank"
+              rel="noopener noreferrer"> 
+              <div className="text-4xl text-blue-500 animate-bounce">
+                <i className="bi bi-facebook"></i>
+              </div>
+            </Link>
+            <Link
+              className="inline-block mx-2"
+              to="https://www.linkedin.com/in/ganesh-mahajan-8553b3224/"
+              target="_blank"
+              rel="noopener noreferrer"> 
+              <div className="text-4xl text-blue-600 animate-bounce">
+                <i className="bi bi-linkedin"></i>
+              </div>
+            </Link>
+            <Link
+              className="inline-block mx-2"
+              to="https://github.com/Ganesh212001"
+              target="_blank"
+              rel="noopener noreferrer"> 
+              <div className="text-4xl text-black animate-bounce">
+                <i className="bi bi-github"></i>
+              </div>
+            </Link>
+            <Link
+              className="inline-block mx-2"
+              to="https://www.instagram.com/ganesh.m_2106/"
+              target="_blank"
+              rel="noopener noreferrer"> 
+              <div className="text-4xl text-pink-700 animate-bounce">
+                <i className="bi bi-instagram"></i>
+              </div>
+            </Link>
+          </div>
         </div>
       </div>
       <About/>
